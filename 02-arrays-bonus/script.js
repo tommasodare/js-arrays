4865323e21e9e89764b4f3f2edbd28ce239a5cfd
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-12 18:39:04 +0100

    Redone Point 2.1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,8 +10,14 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
-console.log(reversedTeachers);
+
+/* const reversedTeachers = teachers.reverse();
+console.log(reversedTeachers); */
+
+for (let index = teachers.length - 1 ; index >= 0; index--) {
+  const reversedTeachers = teachers[index];
+  console.log(reversedTeachers);
+}
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
@@ -26,8 +32,7 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = teachers.filter(Fabio);
-console.log(isFabioPresent);
+const isFabioPresent = null;
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa separata da virgole e salvala nella variabile teachersString
