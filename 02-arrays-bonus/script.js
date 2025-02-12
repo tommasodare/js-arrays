54ae23bb9bc3d65e2ce0914873eac202211643c2
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-12 17:45:24 +0100

    Done Point 2.2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,7 +15,7 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.length;
+const longNames = teachers.filter((teacher) => teacher.length >= 5);
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
@@ -24,10 +24,11 @@ console.log(ed_index);
 teachers.splice(ed_index, 1)
 console.log(teachers);
 
-
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.filter(Fabio);
+console.log(isFabioPresent);
 
-// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+
+// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
