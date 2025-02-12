8b42fb8c5dc882ea0079278600867635c9b854d8
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-12 19:15:02 +0100

    Done Point 2.5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -21,8 +21,18 @@ for (let index = teachers.length - 1 ; index >= 0; index--) {
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.filter((teacher) => teacher.length >= 5);
-console.log(longNames);
+
+/* const longNames = teachers.filter((teacher) => teacher.length >= 5);
+console.log(longNames) */;
+
+for (let index = 0; index < teachers.length; index++) {
+  const longNames = teachers.length[index];
+
+  if (longNames >= 5) {
+    console.log(longNames);
+  }
+
+}
 
 // 3. Rimuovi 'Ed' dall'array teachers
 const ed_index = teachers.indexOf("Ed")
@@ -36,4 +46,5 @@ const isFabioPresent = null;
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.toString();
+console.log(teachersString);
