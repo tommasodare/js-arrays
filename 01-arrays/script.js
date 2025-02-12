7e6c69d68f9642765d0400fc0b7205dd73eb1e04
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-12 16:57:36 +0100

    Done Point 1.8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -42,4 +42,9 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length;
+console.log(isTeachersEmpty);
+
+if (isTeachersEmpty !== 0 ) {
+  console.log("L'array teachers is not empty");
+}
